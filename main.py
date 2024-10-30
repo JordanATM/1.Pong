071bd91ba84ec8f3461b4e645ebdb3f70e825e8a
Jordan <jotorrejon@gmail.com>
2024-10-30 14:34:46 -0300

    Creación de ventana

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import pygame, sys
+
+#General setup
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen_width = 1280
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Pong")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    #Actualizar la pantalla
+
+    pygame.display.flip()
+    clock.tick(60)
